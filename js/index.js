79ed36336aef3ca2075a4dcf77acb720d6a6daa6
M Israk Ahmed <israkahmed.cse@gmail.com>
2025-08-30 02:38:59 -0230

    calling feature added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,43 @@
-const coin= 100;
+// let coin= 20;
 const hearts=0;
+let coin= parseInt(document.getElementById("coin-amount").innerText)
 
+// const cards= document.getElementsByClassName("card")
+// // console.log(cards);
+
+// for ( const eachCard of cards(
+//     document.getel
+    
+// ))
+
+
+document.getElementById("card-container").addEventListener("click",function(event){
+    if(event.target.closest("BUTTON") && coin>=20){
+        // alert("ok")
+
+        const serviceName= event.target.closest("BUTTON").parentNode.parentNode.children[1].innerText;
+        const serviceNumber=event.target.closest("BUTTON").parentNode.parentNode.children[3].innerText;
+
+       alert("You are calling "+ serviceName + ": " + serviceNumber)
+
+
+       coin= coin - 20;
+       document.getElementById("coin-amount").innerText=coin;
+        
+    }
+
+    
+    else if(event.target.tagName == "BUTTON"){     
+         console.log(event.target.tagName);
+    
+    } 
+    else{
+        // alert("You dont have enough balance to make a call.")
+    }
+    
+})
+
+// document.getElementById("btn-call").addEventListener('click',function(event){
+    
+//     alert("ok")
+// })
